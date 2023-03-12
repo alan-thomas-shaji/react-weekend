593319e54556c48dac2333ed77dfe0506f688476
Alan Thomas Shaji <50659499+Captain-shaji-stark@users.noreply.github.com>
2023-03-12 17:23:34 +0530

    navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-transparent shadow-lg">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex-shrink-0">
+            <img className="h-8 w-auto" src={logo} alt="Logo" />
+          </div>
+          <div className="hidden md:block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <div className="nav-item text-purple px-4 hover:text-white">
+                    <span className='p-2'>
+                        <GitHubIcon />
+                    </span>  
+                    Source Code
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
